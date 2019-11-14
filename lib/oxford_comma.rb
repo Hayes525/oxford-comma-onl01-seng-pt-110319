092b4c19305a88814ba3ee9d1a36460ab16a1a33
Jordan Hayes <nosafoundation@gmail.com>
2019-11-14 23:54:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 
 def oxford_comma(array)
-  array
-end 
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    last = array.pop
+    array.join(", ") + ", and " + last
+  end
 end
